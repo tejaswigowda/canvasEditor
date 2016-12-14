946b5f53dac5c646221e3cbbd68c0d8d1505f955
Bikram Baral <baralbikram@hotmail.com>
2016-12-14 14:59:45 -0700

    added fa icons

--- libs/darkroom/darkroom.js
+++ libs/darkroom/darkroom.js
@@ -1,7 +1,15 @@
 ! function() {
     "use strict";
-    var element = document.createElement("div");
-    element.id = "darkroom-icons", element.style.height = 0, element.style.width = 0, element.style.position = "absolute", element.style.visibility = "hidden", element.innerHTML = '<!-- inject:svg --><svg xmlns="http://www.w3.org/2000/svg"><symbol id="close" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/><path d="M0 0h24v24H0z" fill="none"/></symbol><symbol id="crop" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 15h2V7c0-1.1-.9-2-2-2H9v2h8v8zM7 17V1H5v4H1v2h4v10c0 1.1.9 2 2 2h10v4h2v-4h4v-2H7z"/></symbol><symbol id="done" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"/></symbol><symbol id="redo" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M18.4 10.6C16.55 8.99 14.15 8 11.5 8c-4.65 0-8.58 3.03-9.96 7.22L3.9 16c1.05-3.19 4.05-5.5 7.6-5.5 1.95 0 3.73.72 5.12 1.88L13 16h9V7l-3.6 3.6z"/></symbol><symbol id="rotate-left" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M7.11 8.53L5.7 7.11C4.8 8.27 4.24 9.61 4.07 11h2.02c.14-.87.49-1.72 1.02-2.47zM6.09 13H4.07c.17 1.39.72 2.73 1.62 3.89l1.41-1.42c-.52-.75-.87-1.59-1.01-2.47zm1.01 5.32c1.16.9 2.51 1.44 3.9 1.61V17.9c-.87-.15-1.71-.49-2.46-1.03L7.1 18.32zM13 4.07V1L8.45 5.55 13 10V6.09c2.84.48 5 2.94 5 5.91s-2.16 5.43-5 5.91v2.02c3.95-.49 7-3.85 7-7.93s-3.05-7.44-7-7.93z"/></symbol><symbol id="rotate-right" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M15.55 5.55L11 1v3.07C7.06 4.56 4 7.92 4 12s3.05 7.44 7 7.93v-2.02c-2.84-.48-5-2.94-5-5.91s2.16-5.43 5-5.91V10l4.55-4.45zM19.93 11c-.17-1.39-.72-2.73-1.62-3.89l-1.42 1.42c.54.75.88 1.6 1.02 2.47h2.02zM13 17.9v2.02c1.39-.17 2.74-.71 3.9-1.61l-1.44-1.44c-.75.54-1.59.89-2.46 1.03zm3.89-2.42l1.42 1.41c.9-1.16 1.45-2.5 1.62-3.89h-2.02c-.14.87-.48 1.72-1.02 2.48z"/></symbol><symbol id="save" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"/></symbol><symbol id="undo" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12.5 8c-2.65 0-5.05.99-6.9 2.6L2 7v9h9l-3.62-3.62c1.39-1.16 3.16-1.88 5.12-1.88 3.54 0 6.55 2.31 7.6 5.5l2.37-.78C21.08 11.03 17.15 8 12.5 8z"/></symbol></svg><!-- endinject -->', document.body.appendChild(element)
+    /*var element = document.createElement("div");
+    element.id = "darkroom-icons", element.style.height = 0, element.style.width = 0, element.style.position = "absolute", element.style.visibility = "hidden", element.innerHTML = '<div><button id="close"><i class="fa fa-times" aria-hidden="true"></i></button><button id="crop"><i class="fa fa-times"></i></button>' +
+        '<button id="done"><i class="fa fa-times"></i></button>'+
+        '<button id="redo"><i class="fa fa-times"></i></button>'+
+        '<button id="rotate-left"><i class="fa fa-times"></i></button>'+
+        '<button id="rotate-right"><i class="fa fa-times"></i></button>'+
+        '<button id="free-hand"><i class="fa fa-times"></i></button>'+
+        '<button id="save"><i class="fa fa-times"></i></button>'+
+        '<button id="undo"><i class="fa fa-times"></i></button></div>'
+        , document.body.appendChild(element)*/
 }(),
 function() {
     "use strict";
@@ -211,14 +219,16 @@ function() {
         createButton: function(options) {
             var defaults = {
                 image: "help",
+                //iconClass: 'fa fa-check',
                 type: "default",
                 group: "default",
                 hide: !1,
                 disabled: !1
             };
             options = Darkroom.Utils.extend(options, defaults);
+                console.log(options)
             var buttonElement = document.createElement("button");
-            buttonElement.type = "button", buttonElement.className = "darkroom-button darkroom-button-" + options.type, buttonElement.innerHTML = '<svg class="darkroom-icon"><use xlink:href="#' + options.image + '" /></svg>', this.element.appendChild(buttonElement);
+            buttonElement.type = "button", buttonElement.className = "darkroom-button darkroom-button-" + options.type, buttonElement.innerHTML = '<span class="darkroom-icon ' + options.iconClass + '"></span>', this.element.appendChild(buttonElement);
             var button = new Button(buttonElement);
             return button.hide(options.hide), button.disable(options.disabled), button
         }
@@ -319,10 +329,12 @@ function() {
         initialize: function() {
             var buttonGroup = this.darkroom.toolbar.createButtonGroup(),
                 leftButton = buttonGroup.createButton({
-                    image: "rotate-left"
+                    image: "rotate-left",
+                    iconClass: 'fa fa-undo'
                 }),
                 rightButton = buttonGroup.createButton({
-                    image: "rotate-right"
+                    image: "rotate-right",
+                    iconClass: 'fa fa-repeat'
                 });
             leftButton.addEventListener("click", this.rotateLeft.bind(this));
             rightButton.addEventListener("click", this.rotateRight.bind(this));
@@ -342,7 +354,7 @@ function() {
 }(),
 function() {
     "use strict";
-    var Text = Darkroom.Transformation.extend({
+    var FreeHand = Darkroom.Transformation.extend({
         applyTransformation: function(canvas, image, next) {
             var angle = (image.getAngle() + this.options.angle) % 360;
             image.rotate(angle);
@@ -357,27 +369,20 @@ function() {
             next();
         }
     });
-    Darkroom.plugins.rotate = Darkroom.Plugin.extend({
+    Darkroom.plugins.freeHand = Darkroom.Plugin.extend({
         initialize: function() {
             var buttonGroup = this.darkroom.toolbar.createButtonGroup(),
                 leftButton = buttonGroup.createButton({
-                    image: "rotate-left"
-                }),
-                rightButton = buttonGroup.createButton({
-                    image: "rotate-right"
+                    image: "free-hand",
+                    iconClass: 'fa fa-pencil'
+
                 });
-            leftButton.addEventListener("click", this.rotateLeft.bind(this));
-            rightButton.addEventListener("click", this.rotateRight.bind(this));
+            leftButton.addEventListener("click", this.doFreeHand.bind(this));
+            //rightButton.addEventListener("click", this.rotateRight.bind(this));
         },
-        rotateLeft: function() {
-            this.rotate(-90)
-        },
-        rotateRight: function() {
-            this.rotate(90)
-        },
-        rotate: function(angle) {
-            this.darkroom.applyTransformation(new Rotation({
-                angle: angle
+        doFreeHand: function() {
+            this.darkroom.applyTransformation(new FreeHand({
+            
             }))
         }
     })
@@ -466,15 +471,18 @@ function() {
         initialize: function() {
             var buttonGroup = this.darkroom.toolbar.createButtonGroup();
             this.cropButton = buttonGroup.createButton({
-                image: "crop"
+                image: "crop",
+                iconClass: 'fa fa-crop'
             }), this.okButton = buttonGroup.createButton({
                 image: "done",
                 type: "success",
-                hide: !0
+                hide: !0,
+                iconClass: 'fa fa-check'
             }), this.cancelButton = buttonGroup.createButton({
                 image: "close",
                 type: "danger",
-                hide: !0
+                hide: !0,
+                iconClass: 'fa fa-times'
             }), this.cropButton.addEventListener("click", this.toggleCrop.bind(this)), this.okButton.addEventListener("click", this.cropCurrentZone.bind(this)), this.cancelButton.addEventListener("click", this.releaseFocus.bind(this)), this.darkroom.canvas.on("mouse:down", this.onMouseDown.bind(this)), this.darkroom.canvas.on("mouse:move", this.onMouseMove.bind(this)), this.darkroom.canvas.on("mouse:up", this.onMouseUp.bind(this)), this.darkroom.canvas.on("object:moving", this.onObjectMoving.bind(this)), this.darkroom.canvas.on("object:scaling", this.onObjectScaling.bind(this)), fabric.util.addListener(fabric.document, "keydown", this.onKeyDown.bind(this)), fabric.util.addListener(fabric.document, "keyup", this.onKeyUp.bind(this)), this.darkroom.addEventListener("core:transformation", this.releaseFocus.bind(this))
         },
         onObjectMoving: function(event) {
@@ -653,7 +661,9 @@ function() {
         initialize: function() {
             var buttonGroup = this.darkroom.toolbar.createButtonGroup();
             this.destroyButton = buttonGroup.createButton({
-                image: "save"
+                image: "save",
+                iconClass: 'fa fa-floppy-o'
+
             }), this.destroyButton.addEventListener("click", this.options.callback.bind(this))
         }
     })
